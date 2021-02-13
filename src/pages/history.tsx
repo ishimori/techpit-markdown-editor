914dfcda8985ac060636a6edf6174f9ba2968739
ishimori <ishimori@probsc.jp>
2021-02-13 11:16:34 +0900

    4-3の途中まで

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { Header } from "../components/header";
-
+import { getMemos, MemoRecord } from "../indexeddb/memo";
 const HeaderArea = styled.div`
   position: fixed;
   right: 0;
@@ -19,7 +19,33 @@ const Wrapper = styled.div`
   padding: 0 1rem;
 `;
 
+const Memo = styled.button`
+  display: block;
+  background-color: white;
+  border: 1px solid gray;
+  width: 100%;
+  padding: 1rem;
+  margin: 1rem 0;
+  text-align: left;
+`;
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`;
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
 export const History: React.FC = () => {
+  const [memos, setMemos] = useState<MemoRecord[]>([]);
+  useEffect(() => {
+    getMemos().then(setMemos);
+  }, []);
   return (
     <>
       <HeaderArea>
@@ -27,7 +53,15 @@ export const History: React.FC = () => {
           <Link to="editor">エディタに戻る</Link>
         </Header>
       </HeaderArea>
-      <Wrapper>TODO:履歴表示</Wrapper>
+      <Wrapper>
+        TODO:履歴表示
+        {memos.map((memo) => (
+          <Memo key={memo.datetime}>
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
+      </Wrapper>
     </>
   );
 };
